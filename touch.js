419af78175d5f846db5e68ed4944caf6cf3b5864
AbdullahAlmizan644 <abdullahalmizan644@gmail.com>
2022-07-03 01:48:22 +0600

    draw

--- touch.js
+++ touch.js
@@ -8,28 +8,61 @@ canvas.height=window.innerHeight;
 
 let x;
 let y;
+let width;
+let height;
+let startX;
+let startY;
+let isDrawing=false;
 
 
-canvas.addEventListener("touchmove", move);
+canvas.addEventListener("touchstart", startDraw);
+
+canvas.addEventListener("touchmove",draw);
+
+canvas.addEventListener("touchend",endDrawing);
 
 
-function draw(x,y){
-
+function draw(e){
+    e.preventDefault();
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    if(!isDrawing){
+        return;
+    }
+    width=e.touches[0].clientX-startX;
+    height=e.touches[0].clientY-startY;
     ctx.beginPath();
-    ctx.arc(x,y,20,0,2*Math.PI);
-    ctx.fillStyle="red";
-    ctx.fill();
+    // ctx.arc(x,y,11,0,2*Math.PI);
+    ctx.rect(startX,startY,width,height);
+    ctx.stroke();
     ctx.closePath();
+}
+
+
+
+
+function startDraw(e){
+    e.preventDefault();
+    startX=e.touches[0].clientX;
+    startY=e.touches[0].clientY;
+    isDrawing=true;
+
+    
+}
+
+
+function endDrawing(e){
+    e.preventDefault();
+    isDrawing=false;
+
 }
 
 
 function move(e){
     e.preventDefault();
-    console.log(e.touches[0].clientX);
-    // ctx.clearRect(0,0,canvas.width,canvas.height);
-    // x=x+2;
-    // console.log(x);
-    x=e.touches[0].clientX;
-    y=e.touches[0].clientY;
-    draw(x,y);
+    console.log(e.touches[0].radiusX);
+    x=e.touches[0].clientX-width/2;
+    y=e.touches[0].clientY-height/2;
+    width=e.touches[0].radiusX*4;
+    height=e.touches[0].radiusY*4;
+    draw();
 }
